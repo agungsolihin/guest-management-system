b25c45213752c3942b4246ac25065b25e41d9190
mohagungsolihin27 <mohagungsolihin@yahoo.co.id>
2023-09-29 13:45:55 +0700

    connect mongodb

---
+++ controllers/customerController.js
@@ -0,0 +1,57 @@
+const customer = require('../models/customer');
+const mongoose = require('mongoose');
+
+/**
+ * GET /
+ * Homepage
+ */
+
+exports.homepage = async (req, res) => {
+    
+        const locals = {
+            title: 'GSM AAL',
+            description: 'Guest Management System'
+        }
+
+        res.render('index', locals);
+}
+
+/**
+ * GET /
+ * New Customer Form
+ */
+
+exports.addCustomer = async (req, res) => {
+    
+    const locals = {
+        title: 'Add New Customer',
+        description: 'Guest Management System'
+    }
+
+    res.render('customer/add', locals);
+}
+
+/**
+ * POST /
+ * Create New Customer Form
+ */
+
+exports.postCustomer = async (req, res) => {
+
+    console.log(req.body);
+
+    const newCustomer = new customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        details: req.body.details,
+        tel: req.body.tel,
+        email: req.body.email,
+    });
+
+    try {
+        await Customer.create(newCustomer);
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }   
+}
--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./config/db')
 
 const app = express();
 const port = 3000 || process.env.PORT;
+
+// connect to database
+connectDB();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -17,16 +21,14 @@ app.use(expressLayout);
 app.set('layout','./layouts/main');
 app.set('view engine', 'ejs');
 
-//home 
-app.get('/', (req, res) => {
+//router customer
+app.use('/', require('./routers/customer'))
 
-    const locals = {
-        title: 'GSM AAL',
-        description: 'Guest Management System'
-    }
+//handle 404
 
-    res.render('index', locals)
-});
+app.get('*', (req, res) => {
+    res.status(404).render('404');
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}!!!`);
